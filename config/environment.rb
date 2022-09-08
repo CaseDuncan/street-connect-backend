39ae343fe9456c98784bb245032d3fe5b83a0775
Duncan Oluoch <oluochdancun19@gmail.com>
2022-09-08 17:15:45 +0300

    fixing heroku migration bugs

--- config/environment.rb
+++ config/environment.rb
@@ -1,40 +1,26 @@
 require 'bundler/setup'
 Bundler.require
 
- configure :test do
-   ENV['SINATRA_ENV'] ||= "test"
-  
-   require 'bundler/setup'
-   Bundler.require(:default, ENV['SINATRA_ENV'])
-  
-   ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-   )
-  end
+configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
 
- configure :development do
-   ENV['SINATRA_ENV'] ||= "development"
-   require 'bundler/setup'
-   Bundler.require(:default, ENV['SINATRA_ENV'])
-   
-   ActiveRecord::Base.establish_connection(
+  ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
- )
- end
+    :database => "db/street-connect#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
 
 configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 end
 
 require_all 'app'
