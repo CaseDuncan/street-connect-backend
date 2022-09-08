0a9675d63225d8d0c743af930bd6f41ad84e019b
Duncan Oluoch <oluochdancun19@gmail.com>
2022-09-08 16:25:18 +0300

    fixing bugs

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,24 @@
-# This is an _environment variable_ that is used by some of the Rake tasks to determine
-# if our application is running locally in development, in a test environment, or in production
-# ENV['RACK_ENV'] ||= "development"
-
-# # Require in Gems
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['RACK_ENV'])
-
 require 'bundler/setup'
-Bundler.requireconfigure :development do
- ENV['SINATRA_ENV'] ||= "development"require 'bundler/setup'
+Bundler.require
+
+ configure :test do
+   ENV['SINATRA_ENV'] ||= "test"
+  
+   require 'bundler/setup'
+   Bundler.require(:default, ENV['SINATRA_ENV'])
+  
+   ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+   )
+  end
+
+ configure :development do
+ ENV['SINATRA_ENV'] ||= "development
+
+ require 'bundler/setup'
  Bundler.require(:default, ENV['SINATRA_ENV'])
- 
+
  ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
@@ -30,5 +38,4 @@ configure :production do
  )
 end
 
-# Require in all files in 'app' directory
 require_all 'app'
